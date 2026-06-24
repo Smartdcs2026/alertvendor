2eb306aeb8c9d92b02721c64fbcae3217b0a35af
SKDC_SMART <somchaibutphon@gmail.com>
2026-06-24 22:01:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,40 @@
+/**
+ * config.js
+ * การตั้งค่าฝั่ง Frontend
+ *
+ * ห้ามใส่ Secret ทุกชนิดในไฟล์นี้
+ */
+(function (window) {
+  'use strict';
 
+  const API_BASE =
+    'https://alertvendor.somchaibutphon.workers.dev';
+
+  const CONFIG = Object.freeze({
+    APP_NAME:
+      'ระบบติดตามสถานะรถและตู้สินค้า',
+
+    API_BASE:
+      String(API_BASE || '').replace(/\/+$/, ''),
+
+    LOGIN_URL:
+      './login.html',
+
+    DASHBOARD_URL:
+      './index.html',
+
+    API_TIMEOUT_MS:
+      60000,
+
+    AUTH_TIMEOUT_MS:
+      45000,
+
+    TIMEZONE:
+      'Asia/Bangkok',
+
+    DATE_TIME_FORMAT:
+      'dd/MM/yyyy HH:mm:ss'
+  });
+
+  window.APP_CONFIG = CONFIG;
+})(window);
