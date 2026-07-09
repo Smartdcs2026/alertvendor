7a2fa0e5dfc24e6b00333dabcf856fffc2fab742
SKDC_SMART <somchaibutphon@gmail.com>
2026-07-09 18:39:03 +0700

    Update inbound-scanner.js

--- inbound-scanner.js
+++ inbound-scanner.js
@@ -1,6 +1,6 @@
 /************************************************************
  * inbound-scanner.js
- * ROUND 05 HOTFIX 03 — Fast scanner engine
+ * ROUND 05 HOTFIX 04 — Fast scanner engine
  * Native BarcodeDetector + ZXing fallback
  ************************************************************/
 (function (window) {
